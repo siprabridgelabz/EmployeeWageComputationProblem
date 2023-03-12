890c23d513215bfd18e33384b9e5c9e0eb66c5a1
sipra <sipramishra085@gmail.com>
2023-03-12 19:49:01 +0530

    Added file

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace EmployeeWageComputationProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee wage computation problem");
+
+
+
+
+            //EmployeeAttendence.EmpAttendance();
+            //EmployeeCheck.EmployeeWage();
+            // PartTimeEmployee.PartTimeEmployeeWage();
+            // EmployeeWageSwitcCase.EmployeeWageSwitchCaseStatement();
+            // WagesForMonth.WagesforaMonth();
+            EmployeeWageCondition.EmployeeWageConditionForWorkingHours();
+
+
+        }
+    }
+}
