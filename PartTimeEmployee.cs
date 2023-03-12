eb3cc4d9301c18b783118ea1e586a07af503ad67
sipra <sipramishra085@gmail.com>
2023-03-12 20:35:06 +0530

    Added UC3

---
+++ PartTimeEmployee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputationProblem
+{
+    public class PartTimeEmployee
+    {
+        public static void PartTimeEmployeeWage()
+        {
+            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
+
+            Random random = new Random();
+
+            int empCheck = random.Next(2);
+
+            if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+                Console.WriteLine("Employee is present");
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+                Console.WriteLine("Employee is working as part time");
+            }
+            else
+            {
+                empHrs = 0;
+                Console.WriteLine("Employee is absent");
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee Wage : " + empWage);
+        }
+    }
+}
