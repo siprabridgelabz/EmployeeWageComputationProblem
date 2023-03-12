8fe2fd3e4048340da7b4bc7e23794740d7f278ff
sipra <sipramishra085@gmail.com>
2023-03-12 20:00:27 +0530

    Added UC1

---
+++ EmployeeAttendence.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputationProblem
+{
+    public class EmployeeAttendence
+    {
+        public static void EmpAttendance()
+        {
+
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+
+            int empCheck = random.Next(2);
+
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+
+    }
+}
